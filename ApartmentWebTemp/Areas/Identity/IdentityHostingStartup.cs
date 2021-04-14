15a1f2a61db09c50001d5bcbe8e16ce1414af120
Sa-wol <58886408+Sa-wol@users.noreply.github.com>
2021-04-14 09:22:30 -0700

    Scaffolded login and register ASP.NET Identity page

---
+++ ApartmentWebTemp/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ApartmentWebTemp.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(ApartmentWebTemp.Areas.Identity.IdentityHostingStartup))]
+namespace ApartmentWebTemp.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
