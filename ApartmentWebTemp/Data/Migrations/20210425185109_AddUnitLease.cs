c6799b09b9cdd6c33ebceb424c6453dba864dfcd
Sa-wol <58886408+Sa-wol@users.noreply.github.com>
2021-04-25 11:56:44 -0700

    Added migration for UnitLeasing

---
+++ ApartmentWebTemp/Data/Migrations/20210425185109_AddUnitLease.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ApartmentWebTemp.Data.Migrations
+{
+    public partial class AddUnitLease : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UnitLeases",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    PhoneNumber = table.Column<string>(nullable: true),
+                    UnitNumber = table.Column<int>(nullable: false),
+                    UnitFloor = table.Column<int>(nullable: false),
+                    MonthlyRent = table.Column<int>(nullable: false),
+                    UnitDescription = table.Column<string>(nullable: true),
+                    LeaseStarting = table.Column<DateTime>(nullable: false),
+                    LeaseEnding = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UnitLeases", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UnitLeases");
+        }
+    }
+}
