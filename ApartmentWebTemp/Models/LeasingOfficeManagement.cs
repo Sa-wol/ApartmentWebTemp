f844e5b72e356576133aed6bfad867885b09aa7a
Sa-wol <58886408+Sa-wol@users.noreply.github.com>
2021-04-25 12:22:24 -0700

    Added identifying comments and display name

--- ApartmentWebTemp/Models/LeasingOfficeManagement.cs
+++ ApartmentWebTemp/Models/LeasingOfficeManagement.cs
@@ -12,13 +12,20 @@ namespace ApartmentWebTemp.Models
     /// </summary>
     public abstract class LeasingOfficeManagement
     {
+        // Auto-increment unique id number
         [Key]
-        public int Id { get; set; }
+        public int UserId { get; set; }
 
+        // User's first name
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
 
+        // User's last name
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
 
+        // User's phone number
+        [Display(Name = "Phone Number")]
         public string PhoneNumber { get; set; }
     }
 
@@ -28,21 +35,27 @@ namespace ApartmentWebTemp.Models
     public class UnitLease : LeasingOfficeManagement
     {
         // The unit number being occupied
+        [Display(Name = "Unit Number")]
         public int UnitNumber { get; set;}
 
         // The floor number a unit is on
+        [Display(Name = "Unit Floor")]
         public int UnitFloor { get; set; }
 
         // The monthly rent a tenant pays
+        [Display(Name = "Monthly Rent")]
         public int MonthlyRent { get; set; }
 
         // Number of bedroom & bathroom that a unit has
+        [Display(Name = "Unit Description")]
         public string UnitDescription { get; set; }
 
         // The starting lease date for a unit
+        [Display(Name = "Lease Starting Date")]
         public DateTime LeaseStarting { get; set; }
 
         // The ending lease date for a unit
+        [Display(Name = "Lease Ending Date")]
         public DateTime LeaseEnding { get; set; }
     }
 }
