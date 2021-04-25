1e9cecb4bd067dbbe79513f8774a82b51251c747
Sa-wol <58886408+Sa-wol@users.noreply.github.com>
2021-04-25 11:48:46 -0700

    Created leasing office management model

---
+++ ApartmentWebTemp/Models/LeasingOfficeManagement.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApartmentWebTemp.Models
+{
+    /// <summary>
+    /// Leasing office management information
+    /// Landlord has direct access to all information
+    /// </summary>
+    public abstract class LeasingOfficeManagement
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string PhoneNumber { get; set; }
+    }
+
+    /// <summary>
+    /// Tenant's unit information
+    /// </summary>
+    public class UnitLease : LeasingOfficeManagement
+    {
+        // The unit number being occupied
+        public int UnitNumber { get; set;}
+
+        // The floor number a unit is on
+        public int UnitFloor { get; set; }
+
+        // The monthly rent a tenant pays
+        public int MonthlyRent { get; set; }
+
+        // Number of bedroom & bathroom that a unit has
+        public string UnitDescription { get; set; }
+
+        // The starting lease date for a unit
+        public DateTime LeaseStarting { get; set; }
+
+        // The ending lease date for a unit
+        public DateTime LeaseEnding { get; set; }
+    }
+}
