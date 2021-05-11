0749a638a7910845615a2289b7065dd4541394d2
Sa-wol <58886408+Sa-wol@users.noreply.github.com>
2021-05-11 11:07:18 -0700

    Added tenant authorization role to page

--- ApartmentWebTemp/Controllers/TenantPortalController.cs
+++ ApartmentWebTemp/Controllers/TenantPortalController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,6 +7,7 @@ using System.Threading.Tasks;
 
 namespace ApartmentWebTemp.Controllers
 {
+    [Authorize(Roles = "Tenant")]
     public class TenantPortalController : Controller
     {
         public IActionResult Index()
