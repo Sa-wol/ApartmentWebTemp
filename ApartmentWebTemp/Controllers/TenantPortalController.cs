1021906a573f1787b6187552c932db8784ffb948
April <58886408+Sa-wol@users.noreply.github.com>
2021-05-11 11:10:15 -0700

    Issue#22 tenant only portal (#27)
    * Created Tenant portal controller
    
    * Created index and maintenance page for tenant portal controller
    
    * Renamed Tenant Portal file name
    
    * Added basic html to pages
    
    * Added tenant portal link to login side of nav
    
    * Added tenant authorization role to page

---
+++ ApartmentWebTemp/Controllers/TenantPortalController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApartmentWebTemp.Controllers
+{
+    [Authorize(Roles = "Tenant")]
+    public class TenantPortalController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Maintenance()
+        {
+            return View();
+        }
+    }
+}
