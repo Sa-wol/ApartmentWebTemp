d2d6480b9c9e00474ebb9d9bb462c0ffd1bb84c0
April <dunn0056@icloud.com>
2021-06-13 15:55:02 -0700

    Issue#43 home page styling (#44)
    * Added bg img and social media icons
    
    * Changing Amenities page to a one page (Index) website
    
    * Styled header to be the same for everypage.
    Made footer smaller.
    
    * Added CSS to index page
    
    * Added JS to homepage navigation button

--- ApartmentWebTemp/wwwroot/js/site.js
+++ ApartmentWebTemp/wwwroot/js/site.js
@@ -1,4 +1,7 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 
-// Write your JavaScript code.
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    mobile_menu.classList.toggle('active');
+});
